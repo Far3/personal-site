8004c70a335fdd7a2c372104c9426172d9880db0
far3 <franklynroth@gmail.com>
2021-11-27 19:32:53 -0700

    Update youtube link to tech channel

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
       <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
         <div class="leading-tight">Software developer. Always learning.
 </div>
-        <div class="text-green-700 leading-tight"><a href="https://www.youtube.com/channel/UCZiX59V5xFoqLiyfiTo9-9A" target="_blank" rel="noopener">Follow my journey on yotube.</a> </div>
+        <div class="text-green-700 leading-tight"><a href="https://www.youtube.com/channel/UCCIJ09IZaNYCCacVP0YrZMw" target="_blank" rel="noopener">Follow my journey on yotube.</a> </div>
       </div>
       <div class="mt-8 sm:mt-0">
         <g-image src="../../static/me.jpg" alt="hero" class="mx-auto sm:mx-0 image-me" />
@@ -12,7 +12,7 @@
     </div> <!-- end hero -->
 
     <div class="container-inner mx-auto">
-      <p class="text-lg sm:text-xl">Good day! I'm Franklyn. I spend most of my days building enterprise web apps using vue.js react.js and sometimes node.js. I work with large enterprise apps. I'm documenting what I learn to share the latest with the world. Weekly updates on my<a href="https://www.youtube.com/channel/UCZiX59V5xFoqLiyfiTo9-9A" target="_blank" rel="noopener"> channel.</a></p>
+      <p class="text-lg sm:text-xl">Good day! I'm Franklyn. I spend most of my days building enterprise web apps using vue.js react.js and sometimes node.js. I work with large enterprise apps. I'm documenting what I learn to share the latest with the world. Weekly updates on my<a href="https://www.youtube.com/channel/UCCIJ09IZaNYCCacVP0YrZMw" target="_blank" rel="noopener"> channel.</a></p>
   
 
       <div class="flex justify-between items-center py-6">
